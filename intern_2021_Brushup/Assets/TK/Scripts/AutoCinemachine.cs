14ace72f5e23a33765087a6f9b73ee83219089cb
ChinpanGX <xchuhe@gameil.com>
2021-11-01 14:32:55 +0900

    no message

---
+++ intern_2021_Brushup/Assets/TK/Scripts/AutoCinemachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class AutoCinemachine : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField]
+    private float _position;
+
+    private CinemachineTrackedDolly _dolly;
+
+    void Start()
+    {
+        // Virtual Cameraに対してGetCinemachineComponentでCinemachineTrackedDollyを取得する
+        // GetComponentではなくGetCinemachineComponentなので注意
+        _dolly = _virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    void Update()
+    {
+        // パスの位置を更新する
+        _position += 0.005f;
+        // 代入して良いのか不安になる変数名だけどこれでOK
+        _dolly.m_PathPosition = _position;
+    }
+}
---
+++ intern_2021_Brushup/Assets/TM/Script/AuraEffect.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TM
+{
+    public class AuraEffect : MonoBehaviour
+    {
+
+        [Header("タグ・レイヤー設定")]
+        [SerializeField] private GameObject _target;  // 追跡するオブジェクト
+        [SerializeField] private Vector3 _offset; // 地面のレイヤー番号
+        [SerializeField] private SD.TitleStateManager _titleStateManager; // 階級マネージャー
+        [SerializeField] private List<Color> _auraForRanks; // 階級に対する色
+        [SerializeField] private int _lapseFrame; // オーラ消滅までのフレーム
+        [SerializeField] private AnimationCurve _blurCurve_; // ブラーの遷移曲線
+        private int _lapseFrameCnt; // オーラ消滅までのカウント
+        private Material _mat; // オーラマテリアル
+        private int _prevTitleRank;
+
+        private void Start()
+        {
+            var renderer = GetComponentInChildren<Renderer>();
+            _mat = renderer.material;
+            _prevTitleRank = -1;
+        }
+
+        private void FixedUpdate()
+        {
+            int titleRank = _titleStateManager.TitleRank;
+            if (titleRank != _prevTitleRank)
+            {
+                if (_auraForRanks.Count > titleRank)
+                {
+                    var auraColor = _auraForRanks[titleRank];
+                    _mat.SetColor("_Color", auraColor);
+                    _lapseFrameCnt = _lapseFrame;
+                }
+                _prevTitleRank = titleRank;
+            }
+
+            _lapseFrameCnt = System.Math.Max(_lapseFrameCnt - 1, 0);
+            float t = _lapseFrameCnt / (float)_lapseFrame;
+            _mat.SetFloat("_Blur", _blurCurve_.Evaluate(t));
+
+            if (_target)
+            {
+                var targetPos = _target.transform.position;
+
+                transform.position = targetPos + _offset;
+
+                //// 追尾対象の下方向にレイを飛ばして接地点に地震を移動
+                //RaycastHit hit;
+                //if (Physics.Raycast(targetPos, Vector3.down, out hit, Mathf.Infinity, 1 << _groundLayer))
+                //{
+                //    transform.position = hit.point;
+                //}
+            }
+            else
+            {
+                transform.position = new Vector3(99999.0f, 99999.0f, 99999.0f);
+            }
+        }
+    }
+}
