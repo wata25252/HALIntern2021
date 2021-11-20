ba8c0faaf9cb9e6f4d83b7976f6b32ceef94613a
ChinpanGX <xchuhe@gameil.com>
2021-11-20 12:09:56 +0900

    タイトルシーン、リザルトシーンのカメラの動きを調整

--- intern_2021_Brushup/Assets/SD/Scripts/Camera/ResultCameraLookPosition.cs
+++ intern_2021_Brushup/Assets/SD/Scripts/Camera/ResultCameraLookPosition.cs
@@ -20,21 +20,29 @@ namespace SD
         void Start()
         {
             _gm = GameObject.Find("GameManager").GetComponent<TK.GameManager>();
-            _player = GameObject.Find("Player");
+            _player = GameObject.Find("Player/CameraLookPosition");
             _Height = 6.5f;
         }
 
         // Update is called once per frame
         void Update()
         {
-            // 位置をプレイヤーに合わせる
-            Vector3 _playerPosition = _player.transform.position;
-            this.gameObject.transform.position = new Vector3(_playerPosition.x, _Height, _playerPosition.z);
+            
+        }
 
+        private void FixedUpdate()
+        {         
             // ゲームが終了したら、回転する
-            if(_gm.IsGameEnd())
+            if (_gm.IsGameEnd())
             {
                 this.gameObject.transform.Rotate(new Vector3(0.0f, 0.05f, 0.0f));
+            }
+            else
+            {
+                // 位置をプレイヤーに合わせる
+                Vector3 _playerPosition = _player.transform.position;
+                this.gameObject.transform.position = new Vector3(_playerPosition.x, _Height, _playerPosition.z);
+                this.gameObject.transform.rotation = _player.transform.rotation;
             }
         }
     }
