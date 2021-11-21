4dc72b1fd72ffb469849f1fbf984a71822fc04fb
kotera taiki <einszweiguten@gmail.com>
2021-11-21 20:47:42 +0900

    オフライン対応。UIに動きを持たせた

--- intern_2021_Brushup/Assets/TK/Scripts/ViewUi.cs
+++ intern_2021_Brushup/Assets/TK/Scripts/ViewUi.cs
@@ -11,6 +11,7 @@ namespace TK
 
         private bool _isViewing = false;
 
+        private bool _isSelected = false; 
         [SerializeField]
         private string _message;
 
@@ -18,14 +19,27 @@ namespace TK
         {
 
                 var target = GetComponent<Text>();
-                target.DOText(_message, 1.0f);
-
-            
+                target.DOText(_message, 1.0f); 
         }
 
         public void SetMessage(string s)
         {
             _message = s;
         }
+
+        [ContextMenu("HideMessage")]
+        public void HideMessage()
+        {
+            var target = GetComponent<Text>();
+            target.DOText("          ", 0.2f, false, ScrambleMode.Custom, _message);
+        }
+        [ContextMenu("ShowTest")]
+        public void ShowTest()
+        {
+            _message = "TESTTEST";
+            var target = GetComponent<Text>();
+            target.DOText(_message, 1.0f);
+        }
+
     }
 }
--- intern_2021_Brushup/Assets/TK/Scripts/rnking/NCMB_leaderboard.cs
+++ intern_2021_Brushup/Assets/TK/Scripts/rnking/NCMB_leaderboard.cs
@@ -7,37 +7,33 @@ using DG.Tweening;
 
 public class NCMB_leaderboard : MonoBehaviour
 {
-    private NCMB_Ranker _currentRecord;
-    private bool _viewRanking = false;
-    [SerializeField]
-    private GameObject _InputObject;
-    [SerializeField]
-    private GameObject _resultBGM;
-    [SerializeField]
-    private GameObject _gameBGM;
-    [SerializeField]
-    private string _mainSceneName;
-    [SerializeField]
-    private string _titleSceneName;
+    private NCMB_Ranker _currentRecord; //今回の記録
 
-    [SerializeField]
-    private GameObject Fader;
+    [SerializeField] private bool _isOnline = false;//デバッグ用
+    [SerializeField] private GameObject _InputObject; //入力欄
+    [SerializeField] private GameObject _resultBGM; //BGM
+    [SerializeField] private GameObject _gameBGM; //BGM
 
-    [SerializeField]
-    private Text header;
+    //シーン遷移先
+    [SerializeField] private string _mainSceneName;
+    [SerializeField] private string _titleSceneName;
 
-    [SerializeField]
-    private Text[] _top = new Text[5];
-    [SerializeField]
-    private Text[] _top_score = new Text[5];
+    //フェーダー
+    [SerializeField] private GameObject Fader;
 
-    [SerializeField]
-    private List<Text> _options;
+    //表示する文字列
+    [SerializeField] private Text _header;
+    [SerializeField] private Text _failedHeader;
+    [SerializeField] private Text[] _top = new Text[5];
+    [SerializeField] private Text[] _top_score = new Text[5];
 
-    [SerializeField]
-    private int OptionNum;
+    //選択肢
+    [SerializeField] private List<Text> _options;
+    [SerializeField] private List<Text> _failedOptions;
 
-    public List<NCMB_Ranker> _topRankers =null;
+    [SerializeField] private int OptionNum;
+
+    public List<NCMB_Ranker> _topRankers = null;
 
     private bool _isRankIn = false;
 
@@ -50,10 +46,11 @@ public class NCMB_leaderboard : MonoBehaviour
         BEGIN,
         FETCH,
         VIEW,
+        FAILED,
         INPUT,
         SELECT,
     }
-    private RankingState _state=RankingState.BEGIN;
+    [SerializeField] private RankingState _state = RankingState.BEGIN;
 
     private void Start()
     {
@@ -76,6 +73,26 @@ public class NCMB_leaderboard : MonoBehaviour
 
                 }
                 break;
+            case RankingState.FAILED:
+                {
+                    if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                    {
+                        Debug.Log(_failedOptions.Count);
+                        OptionNum = (OptionNum + 1) % (_failedOptions.Count);
+                        _se.Play(4);
+                    }
+                    if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        OptionNum = (OptionNum + _failedOptions.Count - 1) % (_failedOptions.Count);
+                        _se.Play(4);
+                    }
+                    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+                    {
+                        SelectFailedOption();
+                        _se.Play(5);
+                    }
+                }
+                break;
             case RankingState.SELECT:
                 {
 
@@ -90,13 +107,16 @@ public class NCMB_leaderboard : MonoBehaviour
                         OptionNum = (OptionNum + _options.Count - 1) % (_options.Count);
                         _se.Play(4);
                     }
-                    if (Input.GetKeyDown(KeyCode.Return)|| Input.GetKeyDown(KeyCode.Space))
+                    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
                     {
                         SelectOption();
                         _se.Play(5);
                     }
 
                 }
+                break;
+            default:
+                { }
                 break;
         }
 
@@ -117,13 +137,51 @@ public class NCMB_leaderboard : MonoBehaviour
                 break;
             case RankingState.FETCH:
                 {
-                    fetchTopRankers();
-                    _state = RankingState.VIEW;
+                    if (fetchTopRankers()&&_isOnline)
+                    {
+                        for (int i = 0; i < _failedOptions.Count; i++)
+                        {
+                            _failedHeader.GetComponent<TK.ViewUi>().HideMessage();
+
+                            if (_failedOptions[i].text.Length > 0)
+                            {
+                                _failedOptions[i].GetComponent<TK.ViewUi>().HideMessage();
+                            }
+                        }
+                        _state = RankingState.VIEW;
+                    }
+                    else
+                    {
+                        _failedHeader.GetComponent<TK.ViewUi>().ShowWindow();
+
+                        for (int i = 0; i < _failedOptions.Count; i++)
+                        {
+                            _failedOptions[i].GetComponent<TK.ViewUi>().ShowWindow();
+                        }
+                        _state = RankingState.FAILED;
+                    }
+                }
+                break;
+            case RankingState.FAILED:
+                {
+                    //選択された項目の演出
+                    for (int i = 0; i < _failedOptions.Count; i++)
+                    {
+                        if (OptionNum == i)
+                        {
+                            _failedOptions[i].GetComponentInChildren<TK.SelectUi>().SetSelected();
+                        }
+                        else
+                        {
+                            _failedOptions[i].GetComponentInChildren<TK.SelectUi>().SetUnSelected();
+                        }
+
+                    }
+
                 }
                 break;
             case RankingState.VIEW:
                 {
-
                     // ランキングの取得が完了したら1度だけ実行
                     if (_topRankers != null)
                     {
@@ -131,7 +189,7 @@ public class NCMB_leaderboard : MonoBehaviour
                         _topRankers.Sort((a, b) => b.score - a.score);
                         bool first = true;
 
-                        header.GetComponent<TK.ViewUi>().ShowWindow();
+                        _header.GetComponent<TK.ViewUi>().ShowWindow();
                         for (int i = 0; i < 5; i++)
                         {
                             if (_topRankers[i].name == _currentRecord.name && _topRankers[i].score == _currentRecord.score && first)
@@ -156,14 +214,13 @@ public class NCMB_leaderboard : MonoBehaviour
                         }
                         else
                         {
-                            for (int i = 0; i < 2; i++)
+                            for (int i = 0; i < _options.Count; i++)
                             {
                                 _options[i].GetComponent<TK.ViewUi>().ShowWindow();
                             }
                             _state = RankingState.SELECT;
                         }
                     }
-
                 }
                 break;
             case RankingState.INPUT:
@@ -201,6 +258,9 @@ public class NCMB_leaderboard : MonoBehaviour
                     }
                 }
                 break;
+            default:
+                { }
+                break;
         }
     }
     public void SelectOption()
@@ -217,7 +277,29 @@ public class NCMB_leaderboard : MonoBehaviour
                     Fader.GetComponent<IrisIn>().StartFade(_mainSceneName);
                 }
                 break;
+        }
+    }
+    public void SelectFailedOption()
+    {
+        switch (OptionNum)
+        {
+            case 0:
+                {
+                    _state = RankingState.FETCH;
+                }
+                break;
+            case 1:
+                {
+                    Fader.GetComponent<IrisIn>().StartFade(_titleSceneName);
 
+                }
+                break;
+            case 2:
+                {
+                    Fader.GetComponent<IrisIn>().StartFade(_mainSceneName);
+
+                }
+                break;
         }
     }
 
@@ -262,7 +344,7 @@ public class NCMB_leaderboard : MonoBehaviour
         UploadRecord();
     }
 
-    private void fetchTopRankers()
+    private bool fetchTopRankers()
     {
         NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("GameScore");
 
@@ -270,11 +352,13 @@ public class NCMB_leaderboard : MonoBehaviour
 
         query.Limit = 5;
 
+        bool isConnected = true;
         query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
         {
             if (e != null)
             {
                 Debug.Log("検索に失敗しました。エラーコード：" + e.ErrorCode);
+                isConnected = false;
             }
             else
             {
@@ -290,6 +374,8 @@ public class NCMB_leaderboard : MonoBehaviour
                 _topRankers = list;
             }
         });
+
+        return isConnected;
     }
 
 }
