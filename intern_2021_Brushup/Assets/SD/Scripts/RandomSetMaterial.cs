4d7c97b81ee19188ac0cb833444b184de82eff46
ChinpanGX <xchuhe@gameil.com>
2021-12-06 21:30:59 +0900

    MeshRendererのマテリアルの０番目をランダムに変更

---
+++ intern_2021_Brushup/Assets/SD/Scripts/RandomSetMaterial.cs
@@ -0,0 +1,41 @@
+﻿/*-------------------------------------------------------
+ * 
+ *      [RandomSetMaterial.cs]
+ *      Author : 出合翔太
+ *      
+ *      乱数でマテリアルをセットする
+ * 
+ --------------------------------------------------------*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SD
+{
+    public class RandomSetMaterial : MonoBehaviour
+    {
+        [Header("セットしたいマテリアル")]
+        [SerializeField] private Material[] _materialList;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // 変更先のマテリアルを乱数で選ぶ
+            int setMat = Random.Range(0, _materialList.Length);
+
+            // MeshRendererのマテリアル配列の0番目の色を下変更する
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+            // 配列を取得 -> 0番目を変更 -> 配列を返す
+            var tmp = meshRenderer.materials;
+            tmp[0] = _materialList[setMat];
+            meshRenderer.materials = tmp;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
